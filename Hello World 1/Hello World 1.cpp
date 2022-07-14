f73ddd01ecd3c7f03d70972bb1eb28c48a708f75
NomanNasirMinhas <sp17bsse010@gmail.com>
2022-07-14 19:08:02 +0500

    Add project files.

---
+++ Hello World 1/Hello World 1.cpp
@@ -0,0 +1,50 @@
+// Hello World 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //cout << "Hello World!\n";
+    //char sample[] = "NomanNasirMinhas";
+    //cout << sample << " - is wiriting C++\n";
+    //int age;
+    //cout << "Enter Your Age: ";
+    //cin >> age;
+    //cout << "Your age is " << age;
+    //clog << "\nAge Entered";
+    //if (age > 30) {
+    //    cerr << "\nAge Cannot be greater than 30";
+    //}
+
+    ////Clearing Buffer
+    //int num;
+    //char rand_str[80];
+
+    //cout << "Enter Number";
+    //cin >> num;
+    ////cin.ignore(numeric_limits<streamsize>::max(), '\n'); OR
+    ////cin.sync(); OR
+    //cout << "Enter String";
+    //cin >> ws;
+    //cin.getline(rand_str, 80);
+
+    //cout << num << endl;
+    //cout << rand_str << endl;
+    //cout << "Size of rand_str = " << sizeof(rand_str);
+    int test = 1;
+    cout << "First  character " << '1' << endl;
+    cout << "Second character " << (test ? 3 : '1') << endl;
+    return 0;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
